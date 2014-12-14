c45d52844d47670ca9b56af22ed5852e3d81ba8f
colincove <cove.colin@gmail.com>
2014-12-13 20:14:35 -0800

    adding firtst project Xplore

---
+++ Xplore/Assets/Xplore/scripts/char_movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class char_movement : MonoBehaviour {
+	public float speed = 3.0F;
+	public float rotateSpeed = 3.0F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+		transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		float curSpeed = speed * Input.GetAxis("Vertical");
+		controller.SimpleMove(forward * curSpeed);	
+
+
+	}
+
+}
