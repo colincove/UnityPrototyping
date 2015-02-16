177f3f9e22d0a6a7968b2ace56c34925f6ca87dd
colincove <cove.colin@gmail.com>
2015-02-15 17:43:23 -0800

    changes I didn't know I had

---
+++ Xplore/Assets/Xplore/scripts/orb_movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class orb_movement : MonoBehaviour {
+	private float changeDirectionFrequency = 5000;
+	private float lastDirectionChange = 0;
+	private Vector3 force;
+	private int strength = 3;
+	// Use this for initialization
+	void Start () {
+		force = new Vector3 ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.rigidbody.AddForce (force);
+
+		if (lastDirectionChange + changeDirectionFrequency < Time.time * 1000) 
+		{
+			//transform.rigidbody.velocity = Vector3.zero;
+			//transform.rigidbody.angularVelocity  = Vector3.zero;
+
+			lastDirectionChange = Time.time * 1000;
+
+			float a = Random.Range(0, 360);
+
+			force.z = Mathf.Cos(a * (Mathf.PI/180)) * strength;
+			force.x = Mathf.Sin(a * (Mathf.PI/180)) * strength;
+		}
+	}
+}
---
+++ Xplore/Assets/Xplore/scripts/orb_movement_helper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class orb_movement_helper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionStay(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+	}
+}
