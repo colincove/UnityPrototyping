07e31fe7a92851b39984f54a287728de640535a0
colincove <cove.colin@gmail.com>
2015-03-04 21:07:20 -0800

    controls from controller now in. Basic camera and movement started

---
+++ ThePursuitOfHappieness/Assets/ZeldaCamera.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+public var target:GameObject;
+public var pitch:float = 0;
+public var yaw:float = 0;
+public var maxRadius:float = 100;
+private var radius:float = maxRadius;
+function Start () {
+
+}
+
+function Update () {
+	var pos:Vector3 = new Vector3(
+	target.transform.position.x + Mathf.Cos(yaw) * radius, 
+	target.transform.position.y + Mathf.Sin(yaw) * radius,
+	target.transform.position.z
+	);
+	transform.position = pos;
+	yaw += Input.GetAxis("JoystickRightVertical") / 30;
+	pitch += Input.GetAxis("JoystickRightHorizontal") / 30;
+}
---
+++ ThePursuitOfHappieness/Assets/ZeldaMovement.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+private var x_move:float;
+private var y_move:float;
+function Start () {
+
+}
+
+function Update () {
+	x_move = Input.GetAxis("JoystickLeftHorizontal"); 
+	y_move = Input.GetAxis("JoystickLeftVertical"); 
+	rigidbody.transform.Translate(new Vector3(x_move, 0, -y_move));	
+}
